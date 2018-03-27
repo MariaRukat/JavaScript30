b63f7e392e0eca1504ad896b7de70ced22a3ca04
MariaRukat <maria.rukat@gmail.com>
2018-03-27 14:56:23 +0200

    Zadanie01 - po zmianach

--- 01 - JavaScript Drum Kit/app.js
+++ 01 - JavaScript Drum Kit/app.js
@@ -3,36 +3,31 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 
-    var keysDivs = document.querySelectorAll('.key');
-    var audios = document.querySelectorAll('audio');
-
-    window.addEventListener('keydown', function() {
-        var key = event.keyCode;
-
-        for (var i=0; i<keysDivs.length; i++) {
-            if (key == keysDivs[i].dataset.key) {
-                keysDivs[i].classList.add('playing');
-            }
+    var divs = document.querySelectorAll('.key');
+    function removeTransition() {
+        if (event.propertyName !== 'transform') {
+            return
         }
-
-        for (var i=0; i<audios.length; i++) {
-            if (key == audios[i].dataset.key) {
-                audios[i].currentTime = "0";
-                audios[i].play();
-            }
-
-        }
-
-    });
-
-    for (var i=0; i<keysDivs.length; i++) {
-        keysDivs[i].addEventListener('transitionend', function() {
-            if (event.propertyName !== 'transform') {
-                return
-            }
-            this.classList.remove('playing');
-        })
+        this.classList.remove('playing');
     }
+
+    for (var i=0; i<divs.length; i++) {
+        divs[i].addEventListener('transitionend', removeTransition);
+    }
+
+    function playSound() {
+        var key = event.keyCode;
+        var div = document.querySelector(`div[data-key="${key}"]`);
+        var audio = document.querySelector(`audio[data-key="${key}"]`);
+        if (!audio) {
+            return
+        }
+        div.classList.add('playing');
+        audio.currentTime = 0;
+        audio.play();
+    }
+    
+    window.addEventListener('keydown', playSound);
 
 });
 
