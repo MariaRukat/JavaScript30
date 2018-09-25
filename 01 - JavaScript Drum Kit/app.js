3dfb28fd817e3ea1c52cec1750f13ce354202623
MariaRukat <maria.rukat@gmail.com>
2018-09-25 12:35:49 +0200

    Ex.1 - new version

--- 01 - JavaScript Drum Kit/app.js
+++ 01 - JavaScript Drum Kit/app.js
@@ -1,34 +1,23 @@
-//Moja pierwsza wersja rozwiązania
-
-
 document.addEventListener('DOMContentLoaded', function() {
 
-    var divs = document.querySelectorAll('.key');
-    function removeTransition() {
-        console.log(event);
-        if (event.propertyName !== 'transform') {
-            return
-        }
-        this.classList.remove('playing');
-    }
+    const boxes = document.querySelectorAll('.key');
 
-    for (var i=0; i<divs.length; i++) {
-        divs[i].addEventListener('transitionend', removeTransition);
-    }
-
-    function playSound() {
-        var key = event.keyCode;
-        var div = document.querySelector(`div[data-key="${key}"]`);
-        var audio = document.querySelector(`audio[data-key="${key}"]`);
-        if (!audio) {
-            return
-        }
-        div.classList.add('playing');
+    const playing = (e) => {
+        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+        if (!audio) {return}
         audio.currentTime = 0;
         audio.play();
+        const box = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+        box.classList.add('playing');        
     }
 
-    window.addEventListener('keydown', playSound);
+    const removeTransition = (e) => {
+        if (e.propertyName !== 'transform') {return}       
+        e.target.classList.remove('playing');
+    };
+
+    boxes.forEach( box => box.addEventListener('transitionend', removeTransition) );
+    window.addEventListener('keydown', playing);
 
 });
 
